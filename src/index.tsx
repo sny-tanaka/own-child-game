67226618f499ce1f05ae9e365d81199607698c79
sny-tanaka <s05.t12.030@gmail.com>
2025-11-05 14:15:42 +0900

    config

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import reportWebVitals from '@/reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <BrowserRouter basename="/digimon-time-stranger-evolutions">
+  <BrowserRouter basename="/own-child-game">
     <App />
   </BrowserRouter>
 );
